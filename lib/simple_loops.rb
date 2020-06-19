4ab4a3adf19741e4b6980bb7d920ade9e325fc52
Karan S. Chauhan <karanchauhan16@gmail.com>
2020-06-19 15:42:16 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,13 @@
 # Write your methods here
+
+def loop_message_five_times(message)
+   message = "Hello World."
+   counter = 0
+   while counter <= 5 do
+     puts message
+     counter += 1
+   end
+ end
+ 
+  
+  
