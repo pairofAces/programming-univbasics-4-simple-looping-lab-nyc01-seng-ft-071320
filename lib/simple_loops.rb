3ff6aa7bfc82d11da2835fa5a1e51157c8a1a51e
Karan S. Chauhan <karanchauhan16@gmail.com>
2020-06-19 15:52:57 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -11,7 +11,7 @@ def loop_message_five_times(message)
 
 def loop_message_n_times(message, number)
   number = 0
-  message = "Hello Moon."
+  message = "Hello Red Balloon."
   while number <= 5 do
     puts message
     number += 1
