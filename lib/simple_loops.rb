c3cab165f86d8e2e7adc33ee4a190b7bdc90f6bc
Karan S. Chauhan <karanchauhan16@gmail.com>
2020-06-19 16:05:15 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -12,7 +12,7 @@ def loop_message_five_times(message)
 def loop_message_n_times(message, number)
   message = "Hello Moon."
   counter = 0
-  while counter <= number do
+  while counter < number do
     puts message
     counter += 1
   end
